0a17dadd014b03a393eca19eeb9680cf3b459a6a
feranmi <alabiemmanuelferanmi@gmail.com>
2022-08-16 05:50:53 +0100

    update model

--- csv_processing/models.py
+++ csv_processing/models.py
@@ -5,10 +5,20 @@ from .utils import file_upload_directory, generate_processing_id
 # Create your models here.
 
 class FileUploadModel(models.Model):
+    PROCESSING = 0
+    COMPLETED = 1
+    NOT_STARTED = 2
+    PARSE_STATUS = (
+        (PROCESSING, 'Parse Processing'),
+        (COMPLETED, 'Parse completed'),
+        (NOT_STARTED, 'Parse not started'),
+    )
     processing_id = models.CharField(max_length=32, unique=True, editable=False, default=generate_processing_id)
-    file = models.FileField(max_length=255, upload_to=file_upload_directory)
+    file = models.FileField(max_length=255, upload_to=file_upload_directory, null=True)
+    parsed_file = models.FileField(max_length=255, upload_to=file_upload_directory, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    parse_status = models.IntegerField(choices=PARSE_STATUS, default=NOT_STARTED)
 
     def __str__(self) -> str:
         """
