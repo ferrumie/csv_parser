18d6fe675e2c0aecc59ccd9fdff39c0c01370258
feranmi <alabiemmanuelferanmi@gmail.com>
2022-08-16 18:27:25 +0100

    set up docker

--- csv_parser/settings.py
+++ csv_parser/settings.py
@@ -80,16 +80,27 @@ WSGI_APPLICATION = "csv_parser.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "csv_parser",
+#         "USER": "csv_parser",
+#         "PASSWORD": "parse",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "csv_parser",
-        "USER": "csv_parser",
-        "PASSWORD": "parse",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
+
 
 
 # Password validation
