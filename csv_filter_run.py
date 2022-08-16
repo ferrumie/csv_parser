02f00f0e17f5ec51225936580bf8dcbe6e8616e0
feranmi <alabiemmanuelferanmi@gmail.com>
2022-08-16 14:23:58 +0100

    run static validations

--- csv_filter_run.py
+++ csv_filter_run.py
@@ -1,12 +1,13 @@
-from utils.filter_csv import parse_csv
 import sys
+
+from utils.filter_csv import parse_csv
 
 
 def run_processor(filepath):
     """Run the csv parse function."""
     try:
         # get the name of the file
-        file_name = filepath.split('/')[-1]
+        file_name = filepath.split("/")[-1]
         parse_csv(filepath, file_name)
         print("'Output CSV file created'")
     except FileNotFoundError:
@@ -15,6 +16,7 @@ def run_processor(filepath):
         print("File not a valid CSV")
     return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     filepath = sys.argv[1]
     run_processor(filepath)
