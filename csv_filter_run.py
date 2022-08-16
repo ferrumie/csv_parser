de22eaa87894381c95bb7b3d14f34c4852003a44
feranmi <alabiemmanuelferanmi@gmail.com>
2022-08-16 14:23:41 +0100

    create filter run module

---
+++ csv_filter_run.py
@@ -0,0 +1,20 @@
+from utils.filter_csv import parse_csv
+import sys
+
+
+def run_processor(filepath):
+    """Run the csv parse function."""
+    try:
+        # get the name of the file
+        file_name = filepath.split('/')[-1]
+        parse_csv(filepath, file_name)
+        print("'Output CSV file created'")
+    except FileNotFoundError:
+        print("File Not Found")
+    except ValueError:
+        print("File not a valid CSV")
+    return
+
+if __name__ == '__main__':
+    filepath = sys.argv[1]
+    run_processor(filepath)
