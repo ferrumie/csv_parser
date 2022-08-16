ed526ec733dd45ba89b0cb44cfc59d70ba93bb46
feranmi <alabiemmanuelferanmi@gmail.com>
2022-08-16 22:11:27 +0100

    update engine

--- csv_parser/settings.py
+++ csv_parser/settings.py
@@ -86,7 +86,7 @@ django_heroku.settings(locals())
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
