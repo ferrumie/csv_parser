e0dc6a3b65fda308e50c607a4a0a1c6c9e404842
feranmi <alabiemmanuelferanmi@gmail.com>
2022-08-16 05:26:54 +0100

    add filter csv logic

--- csv_processing/views.py
+++ csv_processing/views.py
@@ -2,13 +2,20 @@ from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.parsers import FileUploadParser
 
 from .serializers import CSVFileUploadSerializer
 
 class CSVFileUploadView(APIView):
     serializer_class = CSVFileUploadSerializer
+    parser_classes = [FileUploadParser]
 
     def post(self, request: Request, *args: dict, **kwargs: dict) -> Response:
         """Receives uploaded CSV file."""
         serializer = self.serializer_class(data=request.data, many=True)
-        serializer.is_valid(raise_exception=True)
+        serializer.is_valid(raise_exception=True)   
+
+        # run a celery task to process the file
+        # save the file in the model
+        file = serializer.validated_data['file']
+            
