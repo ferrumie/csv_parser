d946484746a9cdd2728dcdd12a90543046345f46
feranmi <alabiemmanuelferanmi@gmail.com>
2022-08-16 06:36:41 +0100

    set up urls

---
+++ csv_processing/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from .views import CSVFileRetrieveView, CSVFileUploadView, CSVListView
+
+urlpatterns = [
+    path(
+        "upload/",
+        CSVFileUploadView.as_view(),
+        name="api_csv_upload",
+    ),
+    path(
+        "file/<slug:processing_id>",
+        CSVFileRetrieveView.as_view(),
+        name="api_csv_retrieve",
+    ),
+    path(
+        "files/",
+        CSVListView.as_view(),
+        name="api_csv_list",
+    )
+]
