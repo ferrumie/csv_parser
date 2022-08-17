b77e6f9c651a5662177a160447f252e9358454b7
feranmi <alabiemmanuelferanmi@gmail.com>
2022-08-17 01:59:26 +0100

    update to dj_db

--- csv_parser/settings.py
+++ csv_parser/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -94,7 +93,8 @@ DATABASES = {
     }
 }
 
-django_heroku.settings(locals())
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
